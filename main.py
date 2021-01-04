251e595cf2d9f2c09ed78626b9bf9eab5c0aab10
Matteo Hee <matteo.hee@spschools.org>
2021-01-04 19:54:37 +0000

    Senna, Michael, and Naadir ruined everything.

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ print("Your octopus search has taken you away from the rest of your group. You c
 
 print() #line break
 
+
+print("My friend here Justin, he's already taken. And he's cracked at fortnite my guy.")
+print("\nWhat's good people? Im cracked at League of Legends my guy.")
+print("Yeah guys, listen and pay attention.")
+
 print("You swim towards your friends and the mysterious figure. As you get closer, the mysterious figure draws a giant knife.")
 
 print("<Options:>\n<LEAVE your friends to their fate>\n<SWIM as fast as you can to investigate.>\n<SEARCH around for a weapon of your own>\n")
 
-choiceOne = input("What do you?\n>")
-
+choiceOne = ("Cracked at CSGO")
 
 if choiceOne == "LEAVE":
   print("You leave your friends to their fate, swimming off in the other direction. Noone knows what happened to them. You never find an octopus.\n\nGAME OVER")
   exit()
 
-elif choiceOne == "SEARCH":
-  print("You search around wildly for a weapon. You reach toward a large log, but are bit on the finger by a hidden eel.\n\nIt sucks.\n\nGAME OVER")
-  exit()
 
 elif choiceOne == "SWIM":
   print("You swim as fast as you can back to your group. The man smiles at you as you approach. Then, as though realizing for the first time that he is carrying a giant, serrated, rusty knife, he laughs. \"You take the knife\" the man says, as he introduces himself.\nIt turns out, this man is a friendly octopus hunter named \"Big Wes\".")
